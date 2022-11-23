e1a54b44fbb26133a535c5378ad1642ef3498354
Praful Patekar <prafulpatekar23@gmail.com>
2022-11-24 00:03:40 +0300

    Get and Post Operations

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/hello')
 def index():
-    return "Hello World!"
+    return {"message":"Hello World!"}
+
+@app.post('/hello')
+def index2():
+    return "Hi!"
 
 # command to run 
+# if myApp = FastAPI()
+# then => uvicorn main:myApp --reload
 # => uvicorn main:app --reload
