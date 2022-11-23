ff27e3b638d232ff761f042ab05f58b692cbfb52
Praful Patekar <prafulpatekar23@gmail.com>
2022-11-24 00:43:55 +0300

    Section 3 Predefined parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
 
 @app.get('/hello')
@@ -14,6 +14,15 @@ def index2():
 @app.get('/blog/all')
 def blog():
     return {"message":"All Blogs provided!"}
+
+class BlogType(str , Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type:BlogType):
+    return {"message":f"Blog type {type}"}
 
 @app.get('/blog/{id}')
 def blog(id:int):
