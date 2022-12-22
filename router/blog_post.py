a9632c7cb1c896d6484f134b67f303ba7abba558
Praful Patekar <prafulpatekar23@gmail.com>
2022-12-22 09:41:03 +0300

    Parameter section 6

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Query,Body
 from pydantic import BaseModel
 from typing import Optional
 
@@ -16,3 +16,22 @@ class BlogModel(BaseModel):
 @router.post('/new/{id}')
 def create_blog(blog:BlogModel,id:int,version:int=1):
     return {"data":blog,"id":id,"version":version}
+
+
+@router.post('/new/{id}/comment')
+def create_comment(
+        blog:BlogModel,
+        id:int,
+        comment_id:int=Query(None),
+        title='Id for comment',
+        description="Some description",
+        alias="commentId",
+        deprecated=True,
+        content:str=Body(
+                ...,
+                min_length=5,
+                max_length=40,
+                regex='^[a-z\s]*$'
+                )
+        ):
+    return {"blog":blog,"id":id,"comment_id":comment_id,"content":content}
